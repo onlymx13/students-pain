82bad2b198a0f023428d9329aa60213035e3ec28
onlymx13 <onlymx13@gmail.com>
2018-05-16 11:58:35 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,20 @@
 var count = 2;
 var num;
 function main() {
+var br = document.createElement('br');
+document.body.appendChild(br);
+documebt.body.appendChild(br);
 num = document.getElementsByTagName('h1')[0].innerHTML.slice(16);
 var prev = document.createElement('a');
 prev.setAttribute('id','prev');
 prev.innerHTML = 'Previous';
 if (num == 1) {prev.href = '1.html'} else {prev.href = (num - 1) + '.html'};
-document.body.appendChild(prev,null);
+document.body.appendChild(prev);
+document.body.appendChild(br);
+document.body.appendChild(br);
 var next = document.createElement('a');
 next.setAttribute('id','next');
 next.innerHTML = 'Next';
 if (num == count) {next.href = count + '.html'} else {next.href = (num + 1) + '.html'};
-document.body.appendChild(next,null);
+document.body.appendChild(next);
 }
