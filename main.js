6c4b28911bb0f821ff1fef224307371dbf0e8d8a
onlymx13 <onlymx13@gmail.com>
2018-05-16 15:08:11 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,27 +1,32 @@
 function main() {
   const count = 2;
   var num = document.getElementsByTagName('h1')[0].innerHTML.slice(16);
-  var br1 = document.createElement('br'),
-      br2 = document.createElement('br'),
-      br3 = document.createElement('br'),
-      br4 = document.createElement('br');
-  document.body.appendChild(br1);
-  document.body.appendChild(br2);
+  var ul = document.createElement('ul');
+  ul.setAttribute('id','bottom-list');
+  ul.style.listStyleType = 'none';
+  var li1 = document.createElement('li'),
+      li2 = document.createElement('li'),
+      li3 = document.createElement('li');
+  li1.style.float = 'left';
+  li2.style.float = 'left';
+  li3.style.floar = 'left';
   var prev = document.createElement('a');
   prev.setAttribute('id','prev');
   prev.innerHTML = 'Previous';
   if (num == 1) {prev.href = '1.html'} else {prev.href = (Number(num) - 1) + '.html'};
-  document.body.appendChild(prev);
-  document.body.appendChild(br3);
+  li1.appendChild(prev);
   var rand = document.createElement('a');
   rand.setAttribute('id','rand');
   rand.innerHTML = 'Random';
   rand.href = 1 + Math.floor(count * Math.random()) + '.html';
-  document.body.appendChild(rand);
-  document.body.appendChild(br4);
+  li2.appendChild(rand);
   var next = document.createElement('a');
   next.setAttribute('id','next');
   next.innerHTML = 'Next';
   if (num == count) {next.href = count + '.html'} else {next.href = (Number(num) + 1) + '.html'};
-  document.body.appendChild(next);
+  li3.appendChild(next);
+  ul.appendChild(li1);
+  ul.appendChild(li2);
+  ul.appendChild(li3);
+  document.body.appendChild(ul);
 }
