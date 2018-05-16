46bfce2b8900b38ec4aa54f23b39f0ce5c271784
onlymx13 <onlymx13@gmail.com>
2018-05-16 11:12:47 -0500

    Create main.js, 2

---
+++ main.js
@@ -0,0 +1,14 @@
+document.addEventListener('load',main)
+function main() {
+const count = 2;
+var num = document.getElementsByTagName('h1')[0].innerHTML.slice(16);
+var prev = document.createElement('a');
+prev.setAttribute('id','prev');
+prev.innerHTML = 'Previous';
+if (num == 1) {prev.href = '1.html'} else {prev.href = (num - 1) + '.html'};
+document.body.appendChild(prev,null);
+var next = document.createElement('a');
+next.setAttribute('id','next');
+next.innerHTML = 'Next';
+if (num == count) {next.href = count + '.html'} else {next.href = (num + 1) + '.html'};
+}
