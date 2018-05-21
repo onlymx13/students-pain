fc7dbd7dda43eba09f21cfbdba0ca8697b92fbc1
onlymx13 <onlymx13@gmail.com>
2018-05-21 12:57:04 -0500

    Fix fine print

--- main.js
+++ main.js
@@ -37,4 +37,5 @@ function main() {
   var finePrint = document.createElement('p');
   finePrint.style.fontSize = '10px';
   finePrint.innerHTML = "Student's Pain is made by MMMMMMMMMMMMM. Anything inspired directly by xkcd is used under the <a href='https://creativecommons.org/licenses/by-nc/2.5/'>CC BY-NC 2.5 License</a> , but xkcd does not endorse this.";
+  document.body.appendChild(finePrint);
 }
